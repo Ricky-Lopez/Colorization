758e7e61b077dc7854d883fc6e01fd88ee1d4b15
Ricky-Lopez <rllopez718@gmail.com>
2021-04-23 19:21:53 -0400

    Finished debug for k-means algorithm. Works fine now, but could work better.

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def k_means(pxLoad, width, height) :
     clAvg3 = [0,0]
     clAvg4 = [0,0]
     clAvg5 = [0,0]
-    currLowestProximity = width + height
+    currLowestProximity = 2147483647
     sameAvgCounter = 0
+    clusterLoopCounter = 0
 
     #ensures that every pixel chosen for the start of the cluster is different. 
     while(clAvg1 == clAvg2 or clAvg1 == clAvg3 or clAvg1 == clAvg4 or clAvg1 == clAvg5 or clAvg2 == clAvg3 or clAvg2 == clAvg4 or clAvg2 == clAvg5 or clAvg3 == clAvg4 or clAvg3 == clAvg5 or clAvg4 == clAvg5) :
@@ -35,41 +36,63 @@ def k_means(pxLoad, width, height) :
     averages.append(pxLoad[clAvg5[0], clAvg5[1]])
     
     #keeps iterating until the average color of each cluster no longer changes. 
-    while(not(sameAvgCounter == 5)) :
+    while(not(sameAvgCounter == 4)) :
+        sameAvgCounter_old = sameAvgCounter
         sameAvgCounter = 0
         clusters = [ [], [], [], [], [] ]
+
         #Iterates through all pixels, adding them to their respective cluster.
         for i in range(width):
             for j in range(height):
                 for k in range(len(averages)):
-                    proximityRed = math.sqrt((averages[k][0] - pxLoad[i,j][0]) ** 2)
-                    proximityGreen = math.sqrt((averages[k][1] - pxLoad[i,j][1]) ** 2)
-                    proximityBlue = math.sqrt((averages[k][2] - pxLoad[i,j][2]) ** 2)
-                    proximity = math.sqrt((proximityRed + proximityGreen + proximityBlue) ** 2)
+                    proximityRed = abs(averages[k][0] - pxLoad[i,j][0])
+                    proximityGreen = abs(averages[k][1] - pxLoad[i,j][1])
+                    proximityBlue = abs(averages[k][2] - pxLoad[i,j][2])
+                    proximity = math.sqrt((proximityRed ** 2) + (proximityGreen ** 2) + (proximityBlue ** 2))
                     if(proximity < currLowestProximity):
                         currLowestProximity = proximity
                         cluster = k
                 
                 clusters[cluster].append(px[i,j])
-                currLowestProximity = height + width
+                currLowestProximity = 2147483647
         
-        #Calculate the average of each of the clusters and overwrite them. If no cluster averages have been overwritten 
+        #Calculate the average of each of the clusters and overwrite them. If all cluster averages have stayed the same, return averages. 
         for i in range(len(clusters)) :
+            #print(len(clusters[i]))
             redAvg = 0
             greenAvg = 0
             blueAvg = 0
             for k in range(len(clusters[i])) :
-                redAvg += clusters[i][k][0]
-                greenAvg += clusters[i][k][1]
-                blueAvg += clusters[i][k][2]
-            
-            redAvg = redAvg / len(clusters[i])
-            greenAvg = greenAvg / len(clusters[i])
-            blueAvg = blueAvg / len(clusters[i])
-            if(averages[i] == [int(redAvg), int(blueAvg), int(greenAvg)]) :
+                redAvg = redAvg + clusters[i][k][0]
+                greenAvg = greenAvg + clusters[i][k][1]
+                blueAvg = blueAvg + clusters[i][k][2]
+
+            if( len(clusters[i]) ) :
+                redAvg = redAvg / len(clusters[i])
+                greenAvg = greenAvg / len(clusters[i])
+                blueAvg = blueAvg / len(clusters[i])
+
+
+            if(averages[i] == (int(redAvg), int(blueAvg), int(greenAvg))) :
                 sameAvgCounter = sameAvgCounter + 1
-            averages[i] = [int(redAvg), int(blueAvg), int(greenAvg)]
+
+            if( len(clusters[i]) ) : #If there were items in the cluster, then use cluster information and change cluster average.
+                averages[i] = (int(redAvg), int(blueAvg), int(greenAvg))
+            else:
+                sameAvgCounter = sameAvgCounter + 1
+        
+        #CLUSTER LOOP PREVENTION. If cluster averages teeter between numbers for too long, algorithm will terminate.
+        if(sameAvgCounter == sameAvgCounter_old) :
+            clusterLoopCounter += 1
+        else:
+            clusterLoopCounter = 0
+        if(clusterLoopCounter > 50):
+            print(averages)
+            return averages
+
+        print("Clusters Stabilized: " , sameAvgCounter)
     
+    print(averages)
     return averages
 
             
@@ -80,6 +103,14 @@ if __name__ == '__main__' :
     img = Image.open("image_0.jpg") #Full Image
     width, height = img.size
 
+    #Creation of Training Data and Testing Data
+    left_half = (0, 0, width/2, height) #Area tuple of left half
+    right_half = (width/2, 0, width, height) #Area tuple of right half
+    testing_img = img.crop(right_half)
+    training_img = img.crop(left_half)
+    testing_img.save("testing_data.jpg")
+    training_img.save("training_data.jpg")
+
     #Grayscale Code#
     px = img.load()
     for i in range(width):
@@ -88,13 +119,35 @@ if __name__ == '__main__' :
             px[i,j] = (int(avg), int(avg), int(avg))
     img.save("image_0_grayscale.jpg")
 
-    #Creation of Training Data and Testing Data#
-    left_half = (0, 0, width/2, height) #Area tuple of left half
-    right_half = (width/2, 0, width, height) #Area tuple of right half
-    testing_img = img.crop(right_half)
-    training_img = img.crop(left_half)
-    testing_img.save("testing_data.jpg")
+    img = Image.open("image_0.jpg")
+    px = img.load()
+    colors = k_means(px, width, height)
+
+    #Replacing the true colors of the left half of the image with the 5 colors.
+
+    training_img = Image.open("training_data.jpg")
+    width, height = training_img.size
+    px = training_img.load()
+    lowestProx = 766
+
+    #reinitializes every pixel on the left side of the image.
+    for i in range(width):
+        for j in range(height):
+            for k in range(len(colors)) :
+                redProx = math.sqrt((px[i,j][0] - colors[k][0]) ** 2)
+                greenProx = math.sqrt((px[i,j][1] - colors[k][1]) ** 2)
+                blueProx = math.sqrt((px[i,j][2] - colors[k][2]) ** 2)
+                trueProx = math.sqrt((redProx + greenProx + blueProx) ** 2)
+
+                if(trueProx < lowestProx) :
+                    px[i,j] = colors[k]
+                    lowestProx = trueProx
+
     training_img.save("training_data.jpg")
 
-    colors = k_means(px, width, height)
+
+
+    
+
+
 
